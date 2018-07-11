9235d797d4cc19de9b273803b7a44815c6f002d2
Stewart Scott <stewart42@gmail.com>
2018-07-11 22:09:35 +0200

    basic linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">
+          Welcome to React
+        </h1>
       </div>
     );
   }
